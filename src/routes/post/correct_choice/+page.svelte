f6407e5a387e15eec077944f90de972eeedcb2e3
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-12-01 18:12:06 +0100

    .

---
+++ src/routes/post/correct_choice/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let date = "2024-12-01";
+    let title = "What is the correct choice?";
+</script>
+
+<h1 class="text-3xl font-bold mb-1">
+    {title}
+</h1>
+<p class="text-sm mb-4">
+    {date}
+</p>
+
+<!-- POST -->
+<p>
+    - [ ] Write blog post about guessing probability in multiple choice exercises with one-to-one correct answer. 
+</p>
